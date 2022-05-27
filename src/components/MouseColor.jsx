cea12d7c28e531551aac3266138b480f1954781a
unknown <rj346@hotmail.com>
2022-05-28 00:56:21 +0200

    Cambios ejercicio 3

--- src/components/MouseColor.jsx
+++ src/components/MouseColor.jsx
@@ -16,13 +16,38 @@ function MouseColor() {
             height: '255px',
             width: '255px'
         }));
-        console.log(event.clientX);
-        console.log(event.clientY);
+    }
+
+    let capturaEntradaRaton = (event) => {
+        let rojo = ramdomValue();
+        let verde = ramdomValue();
+        let azul = ramdomValue();        
+        setStyle(({
+            backgroundColor: "rgb(" + rojo + ", " + verde + "," + azul + ")",
+            height: '255px',
+            width: '255px'
+        }));
+    }
+    let capturaSalidaRaton = (event) => {
+        let rojo = ramdomValue();
+        let verde = ramdomValue();
+        let azul = ramdomValue();
+        setStyle(({
+            backgroundColor: "rgb(" + rojo + ", " + verde + "," + azul + ")",
+            height: '255px',
+            width: '255px'
+        }));
+    }
+
+    function ramdomValue() {        
+        return Math.floor(Math.random() * (255 - 0) + 1);
     }
 
     return (
         <div style={style}
-        onMouseMove={capturarRaton}>
+        onMouseMove={capturarRaton}
+        onMouseEnter={capturaEntradaRaton} 
+        onMouseLeave={capturaSalidaRaton}>
             Prueba de cambio de color segun movamos el raton por el fondo coloreado
         </div>
     )
